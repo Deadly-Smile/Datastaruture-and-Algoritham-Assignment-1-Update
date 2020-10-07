d684a4a7eed8978bb565f0d4eef3bfc6c6999307
Anik Saha <aniksaha200r@gmail.com>
2020-10-07 17:35:25 +0600

    Update All in one.cpp

--- All in one.cpp
+++ All in one.cpp
@@ -1,9 +1,10 @@
+
 //
 // Created by Anik on 10/6/2020.
 //
 
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+
 using std::cin;
 using std::cout;
 using std::endl;
@@ -13,139 +14,110 @@ private:
     double cradit;
     double result;
 public:
-    Course(double cradit, double result);
-
-    Course();
-
-    void showCourseDetails() const;
-
-    double getCradit() const;
-
-    void setCradit(double courseCradit);
-
-    double getResult() const;
-
-    void setResult(double courseResult);
-};
-
-Course::Course(double cradit, double result)
+    Course(double cradit, double result)
         : cradit(cradit), result(result) {}
 
-Course::Course() {
-    cradit = 0.00;
-    result = 0.00;
-}
+    Course(){
+        cradit = 0.00;
+        result = 0.00;
+    }
+    
+    void showCourseDetails() const{
+        cout<<"Cradit : "<<cradit<<"\n"
+            <<"Result : "<<result<<endl;
+    }
+    
+    double getCradit() const{
+        return cradit;
+    }
 
-double Course::getCradit() const {
-    return cradit;
-}
+    void setCradit(double courseCradit){
+        this->cradit = courseCradit;
+    }
 
-void Course::setCradit(double courseCradit) {
-    Course::cradit = courseCradit;
-}
+    double getResult() const{
+        return result;
+    }
 
-double Course::getResult() const {
-    return result;
-}
-
-void Course::setResult(double courseResult) {
-    Course::result = courseResult;
-}
-
-void Course::showCourseDetails() const {
-    cout<<"Cradit : "<<cradit<<"\n"
-        <<"Result : "<<result<<endl;
-}
+    void setResult(double courseResult){
+        this->result = courseResult;
+    }
+};
 
 class Student {
 private:
     std::vector<Course> courseList;
     int studentID;
 public:
-    Student(int studentID);
+    Student(int studentID) :studentID(studentID) {}
 
-    int getStudentId() const;
+    int getStudentId() const {
+        return studentID;
+    }
 
-    Student(int studentID, short numberOfCourses);
+    Student(int studentID, short numberOfCourses) : studentID(studentID){
+        for (short i = 0; i < numberOfCourses; i++){
+            addCourse();
+        }
+    }
 
-    int getNumberOfCourses() const;
+    int getNumberOfCourses() const {
+        return courseList.size();
+    }
 
-    void addCourse();
+    void addCourse() {
+        Course proxy;
+        double proxyValue;
 
-    void addCourse(const Course& proxy);
+        cout<<"Enter course cradit : ";
+    //    cin.ignore();
+        cin >> proxyValue;
+        proxy.setCradit(proxyValue);
 
-    void showMyCourses();
+        cout<<"Enter course result : ";
+    //    cin.ignore();
+        cin >> proxyValue;
+        proxy.setResult(proxyValue);
 
-    double CalculateCGPA();
+        courseList.push_back(proxy);
+    }
 
-    void setStudentId(int studentId);
+
+    void addCourse(const Course& proxy) {
+        courseList.push_back(proxy);
+    }
+
+    void showMyCourses() {
+        cout<<"Student ID. : "<<studentID<<"\n";
+        for(int i = 0; i < getNumberOfCourses(); i++){
+            cout<<"\nCourse no. "<<i+1<<" :\n";
+            courseList.at(i).showCourseDetails();
+        }
+    }
+
+    double CalculateCGPA() {
+        if(getNumberOfCourses() == 0){
+            return 0.00;
+        }
+        double CGPA;
+        double allCradit{0.00};
+        double earnedCradit{0.00};
+        for(int i = 0; i < getNumberOfCourses(); i++){
+            earnedCradit +=(double) courseList.at(i).getCradit() * courseList.at(i).getResult();
+            allCradit +=(double) courseList.at(i).getCradit();
+        }
+        CGPA = (double ) earnedCradit/allCradit;
+        return CGPA;
+    }
+
+    void setStudentId(int studentId) {
+        studentID = studentId;
+    }
 };
 
-Student::Student(int studentID) :studentID(studentID) {}
-
-Student::Student(int studentID, short numberOfCourses)  : studentID(studentID){
-    for (short i = 0; i < numberOfCourses; i++){
-        addCourse();
-    }
+bool compare(Student student1, Student student2){
+    return student1.CalculateCGPA() < student2.CalculateCGPA();
 }
-
-int Student::getNumberOfCourses() const {
-    return courseList.size();
-}
-
-void Student::addCourse() {
-    Course proxy;
-    double proxyValue;
-
-    cout<<"Enter course cradit : ";
-//    cin.ignore();
-    cin >> proxyValue;
-    proxy.setCradit(proxyValue);
-
-    cout<<"Enter course result : ";
-//    cin.ignore();
-    cin >> proxyValue;
-    proxy.setResult(proxyValue);
-
-    courseList.push_back(proxy);
-}
-
-void Student::addCourse(const Course& proxy) {
-    courseList.push_back(proxy);
-}
-
-void Student::showMyCourses() {
-    cout<<"Student ID. : "<<studentID<<"\n";
-    for(int i = 0; i < getNumberOfCourses(); i++){
-        cout<<"\nCourse no. "<<i+1<<" :\n";
-        courseList.at(i).showCourseDetails();
-    }
-}
-
-double  Student::CalculateCGPA() {
-    if(getNumberOfCourses() == 0){
-        return 0.00;
-    }
-    double CGPA;
-    double allCradit{0.00};
-    double earnedCradit{0.00};
-    for(int i = 0; i < getNumberOfCourses(); i++){
-        earnedCradit +=(double) courseList.at(i).getCradit() * courseList.at(i).getResult();
-        allCradit +=(double) courseList.at(i).getCradit();
-    }
-    CGPA = (double ) earnedCradit/allCradit;
-    return CGPA;
-}
-
-int Student::getStudentId() const {
-    return studentID;
-}
-
-void Student::setStudentId(int studentId) {
-    studentID = studentId;
-}
-
-bool compare(Student student1, Student student2);
 
 int main() {
     int numberOfStudent{0};
@@ -181,8 +153,4 @@ int main() {
     }
 
     return 0;
-}
-
-bool compare(Student student1, Student student2){
-    return student1.CalculateCGPA() < student2.CalculateCGPA();
 }
